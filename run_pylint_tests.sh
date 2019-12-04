f0ec30e354667c4165bb6d49676c6dadea6bcd6a
jc1204-png <jc@cmms.ma>
2019-12-04 12:56:24 +0100

    shellcheck run_pylint_tests.sh

--- run_pylint_tests.sh
+++ run_pylint_tests.sh
@@ -1,16 +1,28 @@
 #!/bin/sh
 
-
-# $ADDONS_PATH
-# $EXIT_ZERO
-# $RY
 set +e
 
+green() {
+  printf  "\\e[32m${1}\\e[0m"
+}
+
+red() {
+  printf  "\\e[31m${1}\\e[0m"
+}
+
+MODULES=$(find "$ADDONS_PATH"/* -maxdepth 0 -type d -and -not -path \*/.\* -print0 | xargs -0 echo)
 
 echo "-------------------------------------------------"
+echo "Modules to test :"
+green "$MODULES"
 echo
-echo "TEST: pylint"
-pylint --rcfile=/root/.pylintrc --load-plugins=pylint_odoo --exit-zero -ry -e epylint $(ls -ad $ADDONS_PATH/*/ | sed s'/.$//'| xargs echo)
+if [ -z "$MODULES" ]
+then
+	red "Modules list is empty"
+	exit 1
+fi
+
+pylint --rcfile=/root/.pylintrc --load-plugins=pylint_odoo --exit-zero -ry -e epylint $MODULES
 
 exit $?
 
